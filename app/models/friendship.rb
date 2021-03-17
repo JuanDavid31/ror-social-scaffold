0a5077df8a47b257b3b9662f6091844654d8c9af
Juan David <juandavid0306@hotmail.com>
2021-03-17 17:40:16 -0500

    Add after_save callback on friendship model

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,6 +1,13 @@
 class Friendship < ApplicationRecord
   validates :status, presence: true
 
+  after_save :create_opposite
+
   belongs_to :user1, class_name: 'User', foreign_key: :user_1
   belongs_to :user2, class_name: 'User', foreign_key: :user_2
+
+  def create_opposite
+    puts 'after_save'
+    Friendship.create(user1: user_2, user2: user_1, status: status)
+  end
 end
