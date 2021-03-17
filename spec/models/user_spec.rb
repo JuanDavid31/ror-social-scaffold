bf984c4be0ffa05eada3240a0f28e37a79e5b749
Juan David <juandavid0306@hotmail.com>
2021-03-17 14:05:07 -0500

    Add user_spec.rb

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+  let(:juan) { User.new(name: 'Juan', email: 'johndoe1@sample.com', password: '12345678') }
+
+  describe '#valid?' do
+    it 'does validate all the user variables' do
+      expect(User.new.valid?).to be false
+    end
+
+    it 'does validate the user name' do
+      expect(juan.valid?).to be true
+    end
+  end
+end
