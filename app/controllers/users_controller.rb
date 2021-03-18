c2710761e3e63d1de4d02794addbc09b36bfa6e2
Juan David <juandavid0306@hotmail.com>
2021-03-18 12:52:22 -0500

    Add send_friend_request feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  helper_method :send_friend_request_path
 
   def index
     @users = User.all
@@ -8,5 +9,19 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.ordered_by_most_recent
+
+    @is_friend = current_user.friends.one? { |friend| friend.id == @user.id }
+    @request_sent = @user.friend_responses.one? { |u| u.id == current_user.id }
+  end
+
+  def send_friend_request
+    user_id = params[:user_id]
+    user_2 = User.find user_id
+    Friendship.create(user1: current_user, user2: user_2, status: 'Pending')
+    redirect_to user_path id: user_id
+  end
+
+  def send_friend_request_path(user)
+    "/send_friend_request?user_id=#{user.id}"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
     resources :likes, only: [:create, :destroy]
   end
 
+  post '/send_friend_request', to: 'users#send_friend_request'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
