1db3b0398ad71c0b8d2e416c4afbdbf12301cb2b
Juan David <juandavid0306@hotmail.com>
2021-03-18 11:27:29 -0500

    Add friends, pending_friend_resquests/repsonses methods with their respective specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,35 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_many :friendships
+
+  def friends
+    sql = "select u2.*
+            from users u1
+            inner join friendships f on u1.id = f.user_1
+            inner join users u2 on u2.id = f.user_2
+            where status = 'Accepted'
+            and u1.id = ?"
+    User.find_by_sql [sql, id]
+  end
+
+  def pending_friend_requests
+    sql = "select u2.*
+            from users u1
+            inner join friendships f on u1.id = f.user_1
+            inner join users u2 on u2.id = f.user_2
+            where status = 'Pending'
+            and u1.id = ?"
+    User.find_by_sql [sql, id]
+  end
+
+  def pending_friend_responses
+    sql = "select u2.*
+            from users u1
+            inner join friendships f on u1.id = f.user_1
+            inner join users u2 on u2.id = f.user_2
+            where status = 'Pending'
+            and u2.id = ?"
+    User.find_by_sql [sql, id]
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:juan) { User.new(name: 'Juan', email: 'johndoe1@sample.com', password: '12345678') }
+  let(:valid_user) { User.new(name: 'Johndoe', email: 'johndoe123@sample.com', password: '12345678') }
+
+  fixtures :all
 
   describe '#valid?' do
     it 'does validate all the user variables' do
@@ -9,7 +11,30 @@ RSpec.describe User, type: :model do
     end
 
     it 'does validate the user name' do
-      expect(juan.valid?).to be true
+      expect(valid_user.valid?).to be true
+    end
+  end
+
+  context 'When the user has friends, pending friend requests and/or pending friend responses' do
+    describe '#friends' do
+      it 'does return the current friends' do
+        user = users(:juan)
+        expect(user.friends.length).to be 1
+      end
+    end
+
+    describe '#pending_friend_requests' do
+      it 'does return the pending friend requests that the user made' do
+        user = users(:juan)
+        expect(user.pending_friend_requests.length).to be 1
+      end
+    end
+
+    describe '#pending_friend_responses' do
+      it 'does return the pending friend responses that the user has' do
+        user = users(:juandavid)
+        expect(user.pending_friend_responses.length).to be 1
+      end
     end
   end
 end
