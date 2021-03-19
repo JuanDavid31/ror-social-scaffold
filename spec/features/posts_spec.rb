8f9b6996abfa7a2e83feb6abf4ab940281ba2f75
Juan David <juandavid0306@hotmail.com>
2021-03-19 15:21:31 -0500

    Add integration specs for listing friend requests, accepting, and rejecting

--- spec/features/posts_spec.rb
+++ spec/features/posts_spec.rb
@@ -13,29 +13,39 @@ RSpec.describe Post, type: :feature do
   end
 
   context 'When a user is logged in' do
-    it 'its possible to see every friends posts and owning posts ' do
+    it 'does see every friends posts and owning posts ' do
       sign_in users(:juan)
       visit root_url
       expect(page).to have_content 'This is quality content'
       expect(page).to have_content 'More quality content'
     end
 
-    it 'does send friend requests' do # See sent friend requests
+    it 'does send friend requests' do
       sign_in users(:arnold2)
       visit user_path(id: users(:juandavid).id)
       click_button 'Send friend request'
       expect(page).to have_content 'Friend request sent'
     end
+
+    it 'does see friend requests' do
+      sign_in users(:juan)
+      visit '/friend_requests'
+      expect(page).to have_content 'Juan David'
+      expect(page).to have_content 'Pending'
+    end
+
+    it 'does accept a friend request' do
+      sign_in users(:juandavid)
+      visit '/friend_responses'
+      click_link 'Accept'
+      expect(page).to_not have_content 'Pending'
+    end
+
+    it 'does reject a friend request' do
+      sign_in users(:juandavid)
+      visit '/friend_responses'
+      click_link 'Reject'
+      expect(page).to have_content 'Rejected'
+    end
   end
-
-
-
-
-    # See received friend requests
-
-    # Send friend request
-
-    # Accept friend request
-
-    # Reject friend request
 end
